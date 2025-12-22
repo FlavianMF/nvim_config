2b00a6caa066cbaa7a3d507864fcda0a63ed103f
Flavian Melquiades Fernandes <flavian.fernandes@fit-tecnologia.org.br>
2025-12-22 16:02:50 -0300

    feat: open flash, monitor and "D" in tmux panels

--- lua/plugins/idf.lua
+++ lua/plugins/idf.lua
@@ -19,7 +19,7 @@ return {
           local command = "idf.py build"
           local in_tmux = vim.fn.exists("$TMUX") == 1
           if in_tmux then
-            local pane_id = vim.g.idf_build_pane_id
+            local pane_id = vim.g.idf_cmd_pane_id
             local pane_exists = false
             if pane_id then
               -- check if pane exists
@@ -43,7 +43,7 @@ return {
               -- create new pane
               local new_pane_cmd = "tmux split-window -d -P -F '#D'" -- -d to not switch to it
               local new_pane_id = vim.fn.trim(vim.fn.system(new_pane_cmd))
-              vim.g.idf_build_pane_id = new_pane_id
+              vim.g.idf_cmd_pane_id = new_pane_id
               local final_cmd = string.format(
                 "tmux send-keys -t %s '%s; echo; echo \"Press Enter to close panel...\"; read' Enter",
                 new_pane_id,
@@ -62,23 +62,162 @@ return {
       {
         "<C-e>f",
         function()
-          require("esp32").command("flash")
+          local command = "idf.py flash"
+          local in_tmux = vim.fn.exists("$TMUX") == 1
+          if in_tmux then
+            local pane_id = vim.g.idf_cmd_pane_id
+            local pane_exists = false
+            if pane_id then
+              -- check if pane exists
+              vim.fn.system(string.format("tmux list-panes -F '#D' | grep -q '^%s$'", pane_id))
+              if vim.v.shell_error == 0 then
+                pane_exists = true
+              end
+            end
+
+            if pane_exists then
+              vim.fn.system(string.format("tmux send-keys -t %s C-c", pane_id))
+              vim.fn.system(string.format("tmux send-keys -t %s 'clear' Enter", pane_id))
+              local final_cmd = string.format(
+                "tmux send-keys -t %s '%s; echo; echo \"Press Enter to close panel...\"; read' Enter",
+                pane_id,
+                command
+              )
+              vim.fn.system(final_cmd)
+              vim.fn.system(string.format("tmux select-pane -t %s", pane_id))
+            else
+              -- create new pane
+              local new_pane_cmd = "tmux split-window -d -P -F '#D'" -- -d to not switch to it
+              local new_pane_id = vim.fn.trim(vim.fn.system(new_pane_cmd))
+              vim.g.idf_cmd_pane_id = new_pane_id
+              local final_cmd = string.format(
+                "tmux send-keys -t %s '%s; echo; echo \"Press Enter to close panel...\"; read' Enter",
+                new_pane_id,
+                command
+              )
+              vim.fn.system(final_cmd)
+              vim.fn.system(string.format("tmux select-pane -t %s", new_pane_id))
+            end
+          else
+            -- Fallback for non-tmux environments
+            require("esp32").command("flash")
+          end
         end,
         desc = "IDF: Flash Project",
       },
       {
         "<C-e>m",
         function()
-          require("esp32").command("monitor")
+          local command = "idf.py monitor"
+          local in_tmux = vim.fn.exists("$TMUX") == 1
+          if in_tmux then
+            local pane_id = vim.g.idf_cmd_pane_id
+            local pane_exists = false
+            if pane_id then
+              -- check if pane exists
+              vim.fn.system(string.format("tmux list-panes -F '#D' | grep -q '^%s$'", pane_id))
+              if vim.v.shell_error == 0 then
+                pane_exists = true
+              end
+            end
+
+            if pane_exists then
+              vim.fn.system(string.format("tmux send-keys -t %s C-c", pane_id))
+              vim.fn.system(string.format("tmux send-keys -t %s 'clear' Enter", pane_id))
+              local final_cmd = string.format("tmux send-keys -t %s '%s' Enter", pane_id, command)
+              vim.fn.system(final_cmd)
+              vim.fn.system(string.format("tmux select-pane -t %s", pane_id))
+            else
+              -- create new pane
+              local new_pane_cmd = "tmux split-window -d -P -F '#D'" -- -d to not switch to it
+              local new_pane_id = vim.fn.trim(vim.fn.system(new_pane_cmd))
+              vim.g.idf_cmd_pane_id = new_pane_id
+              local final_cmd = string.format("tmux send-keys -t %s '%s' Enter", new_pane_id, command)
+              vim.fn.system(final_cmd)
+              vim.fn.system(string.format("tmux select-pane -t %s", new_pane_id))
+            end
+          else
+            -- Fallback for non-tmux environments
+            require("esp32").command("monitor")
+          end
         end,
         desc = "IDF: Monitor Device",
       },
       {
         "<C-e>a",
         function()
-          require("esp32").command("flash monitor")
+          local command = "idf.py flash monitor"
+          local in_tmux = vim.fn.exists("$TMUX") == 1
+          if in_tmux then
+            local pane_id = vim.g.idf_cmd_pane_id
+            local pane_exists = false
+            if pane_id then
+              -- check if pane exists
+              vim.fn.system(string.format("tmux list-panes -F '#D' | grep -q '^%s$'", pane_id))
+              if vim.v.shell_error == 0 then
+                pane_exists = true
+              end
+            end
+
+            if pane_exists then
+              vim.fn.system(string.format("tmux send-keys -t %s C-c", pane_id))
+              vim.fn.system(string.format("tmux send-keys -t %s 'clear' Enter", pane_id))
+              local final_cmd = string.format("tmux send-keys -t %s '%s' Enter", pane_id, command)
+              vim.fn.system(final_cmd)
+              vim.fn.system(string.format("tmux select-pane -t %s", pane_id))
+            else
+              -- create new pane
+              local new_pane_cmd = "tmux split-window -d -P -F '#D'" -- -d to not switch to it
+              local new_pane_id = vim.fn.trim(vim.fn.system(new_pane_cmd))
+              vim.g.idf_cmd_pane_id = new_pane_id
+              local final_cmd = string.format("tmux send-keys -t %s '%s' Enter", new_pane_id, command)
+              vim.fn.system(final_cmd)
+              vim.fn.system(string.format("tmux select-pane -t %s", new_same_id))
+            end
+          else
+            -- Fallback for non-tmux environments
+            require("esp32").command("flash monitor")
+          end
         end,
         desc = "IDF: Flash & Monitor",
+      },
+      {
+        "<C-e>d",
+        function()
+          local command = "idf.py build && idf.py flash monitor"
+          local in_tmux = vim.fn.exists("$TMUX") == 1
+          if in_tmux then
+            local pane_id = vim.g.idf_cmd_pane_id
+            local pane_exists = false
+            if pane_id then
+              -- check if pane exists
+              vim.fn.system(string.format("tmux list-panes -F '#D' | grep -q '^%s$'", pane_id))
+              if vim.v.shell_error == 0 then
+                pane_exists = true
+              end
+            end
+
+            if pane_exists then
+              vim.fn.system(string.format("tmux send-keys -t %s C-c", pane_id))
+              vim.fn.system(string.format("tmux send-keys -t %s 'clear' Enter", pane_id))
+              local final_cmd = string.format("tmux send-keys -t %s '%s' Enter", pane_id, command)
+              vim.fn.system(final_cmd)
+              vim.fn.system(string.format("tmux select-pane -t %s", pane_id))
+            else
+              -- create new pane
+              local new_pane_cmd = "tmux split-window -d -P -F '#D'" -- -d to not switch to it
+              local new_pane_id = vim.fn.trim(vim.fn.system(new_pane_cmd))
+              vim.g.idf_cmd_pane_id = new_pane_id
+              local final_cmd = string.format("tmux send-keys -t %s '%s' Enter", new_pane_id, command)
+              vim.fn.system(final_cmd)
+              vim.fn.system(string.format("tmux select-pane -t %s", new_pane_id))
+            end
+          else
+            -- Fallback for non-tmux environments
+            require("esp32").command("build flash monitor")
+          end
+        end,
+        desc = "IDF: Build, Flash & Monitor",
       },
       {
         "<C-e>c",
